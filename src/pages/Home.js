ced3cfeef7684916b4d784e3def9b63d47465c92
Marcelo Bilar Rodrigues <marcelo.bilar2@gmail.com>
2021-08-06 13:21:57 -0300

    Criação do Header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import {Header} from '../componentes/Header';
 
 export const Home = props => {
+
+    const sair = () => {
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('usuarioNome');
+        localStorage.removeItem('usuarioEmail');
+        props.setAccessToken('');
+    }
+
     return (
-        <h1>Gerenciador de tarefas - Home</h1>
+        <>
+            <Header sair={sair}/>
+        </>
     )
 }
